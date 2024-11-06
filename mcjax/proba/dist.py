16788a0a333a300d825ee97d79579d62ec7a98b9
Alex Thiery <1282389+alexxthiery@users.noreply.github.com>
2024-11-07 00:19:16 +0800

    added neal's funnel + reorganize notebooks

--- mcjax/proba/dist.py
+++ mcjax/proba/dist.py
@@ -1,16 +1,20 @@
-# abtract class for a target distribution
-from abc import ABC, abstractmethod
+#from abc import ABC, abstractmethod
 import jax
+from typing import Callable
 
 
 # ==================================
-# Abstract class for a distribution
+# class for a general distribution
 # ===================================
-class Dist(ABC):
-    @abstractmethod
-    def logpdf(self, x):
-        """ unormalized logpdf of the distribution """
-        pass
+class Dist:
+    def __init__(
+                self,
+                *,
+                logpdf: Callable,   # logpdf of the distribution
+                dim: int            # dimension of the distribution
+                ):
+        self.logpdf = logpdf
+        self._dim = dim
 
     def logpdf_batch(self, x_batch):
         """ logpdf of the distribution for a batch of samples """
@@ -23,21 +27,21 @@ class Dist(ABC):
 
 
 # ==================================
-# Abstract class for a differentiable distribution
+# class for a differentiable distribution
 # Gradient of the distribution is required
 # ===================================
 class DiffDist(Dist):
-    def logpdf_grad(self, x):
+    def grad(self, x):
         """ gradient of the distribution """
         return jax.grad(self.logpdf)(x)
 
     def grad_batch(self, x_batch):
         """ gradient of the distribution for a batch of samples """
-        return jax.vmap(self.logpdf_grad)(x_batch)
+        return jax.vmap(self.grad)(x_batch)
 
     def logpdf_and_grad(self, x):
         """ logpdf and gradient of the distribution """
-        return self.logpdf(x), self.logpdf_grad(x)
+        return self.logpdf(x), self.grad(x)
 
     def logpdf_and_grad_batch(self, x_batch):
         """ logpdf and gradient of the distribution for a batch of samples """
--- mcjax/proba/neal_funnel.py
+++ mcjax/proba/neal_funnel.py
@@ -4,24 +4,27 @@ import jax.random as jr
 
 
 # ==================================
-# Banana 2D Distribution
+# Neal's Funnel Distribution
+# x ~ N(0, sigma_x^2), y ~ N(0, variance=exp(x))
+# with sigma_x = 3
 # ==================================
-class Banana2D(DiffDist):
-    """ Banana 2D Distribution:
-     target(x,y) \propto exp{ -0.5*( (y-x^2)^2/noise_std^2 + (x-1)^2 ) }
-    remark: with noise_std = 0.1, the -logpdf is the Rosenbrock function
+class NealFunnel(DiffDist):
+    """ Neal's Funnel Distribution
+    x ~ N(0, sigma_x^2), y ~ N(0, exp(x/2))
+    with sigma_x = 3
     """
     def __init__(
                 self,
                 *,
-                noise_std=0.1,    # noise standard deviation
+                sigma_x=3.,    # noise standard deviation
                 ):
-        self.noise_std = noise_std
-
+        self.sigma_x = sigma_x
+        
         # define the logpdf
         def logpdf(x):
             x0, x1 = x[0], x[1]
-            return -0.5*((x0 - 1.)**2 + (x1 - x0**2)**2 / self.noise_std**2)
+            std = jnp.exp(x0/2.)
+            return -0.5*(x0/self.sigma_x)**2 - 0.5*(x1/std)**2 - 0.5*jnp.log(std**2)
 
         # call the parent class
         super().__init__(logpdf=logpdf, dim=2)
@@ -29,10 +32,10 @@ class Banana2D(DiffDist):
     def sample(self, key, n_samples):
         # samples x0_s
         key, key_ = jr.split(key)
-        x0_s = 1. + jr.normal(key_, (n_samples,))
+        x0_s = self.sigma_x * jr.normal(key_, (n_samples,))
         # samples x1_s
         key, key_ = jr.split(key)
-        x1_s = x0_s**2 + self.noise_std * jr.normal(key_, (n_samples,))
+        stds = jnp.exp(x0_s/2.)
+        x1_s = stds * jr.normal(key_, (n_samples,))
         return jnp.stack([x0_s, x1_s], axis=-1)
-
     
