d7452d833a5cdf8531e4f21d2fbf37d8ee24ee38
Alex Thiery <1282389+alexxthiery@users.noreply.github.com>
2024-11-07 19:53:50 +0800

    multidimensional Neal's funnel

--- mcjax/proba/neal_funnel.py
+++ mcjax/proba/neal_funnel.py
@@ -4,36 +4,43 @@ import jax.random as jr
 
 
 # ==================================
-# Neal's Funnel Distribution
-# x ~ N(0, sigma_x^2), y ~ N(0, variance=exp(x))
-# with sigma_x = 3
+# D-dimensional Neal's Funnel Distribution
+# x0 ~ N(0, sigma_x^2),
+# xi ~ N(0, variance=exp(x0)) are independent for i=1,...,D-1
+# By default:
+#   sigma_x = 3
+#   D = 2
 # ==================================
 class NealFunnel(LogDensity):
     """ Neal's Funnel Distribution
-    x ~ N(0, sigma_x^2), y ~ N(0, exp(x/2))
-    with sigma_x = 3
+    x0 ~ N(0, sigma_x^2),
+    xi ~ N(0, variance=exp(x0)) are independent for i=1,...,D-1
+    By default:
+    sigma_x = 3
+    D = 2
     """
     def __init__(
                 self,
                 *,
                 sigma_x=3.,    # noise standard deviation
+                dim=2,         # dimension
                 ):
         self.sigma_x = sigma_x
-        self._dim = 2
+        self._dim = dim
 
     # define the logpdf
     def logdensity(self, x):
-        x0, x1 = x[0], x[1]
+        x0, x1 = x[0], x[1:]
         std = jnp.exp(x0/2.)
-        return -0.5*(x0/self.sigma_x)**2 - 0.5*(x1/std)**2 - 0.5*jnp.log(std**2)
+        return -0.5*(x0/self.sigma_x)**2 - 0.5*jnp.sum(x1/std)**2 - 0.5*(self.dim-1)*jnp.log(std**2)
 
     def sample(self, key, n_samples):
         # samples x0_s
         key, key_ = jr.split(key)
-        x0_s = self.sigma_x * jr.normal(key_, (n_samples,))
+        x0_s = self.sigma_x * jr.normal(key_, (n_samples, 1))
         # samples x1_s
         key, key_ = jr.split(key)
         stds = jnp.exp(x0_s/2.)
-        x1_s = stds * jr.normal(key_, (n_samples,))
-        return jnp.stack([x0_s, x1_s], axis=-1)
+        x1_s = stds * jr.normal(key_, (n_samples, self.dim-1))
+        return jnp.concatenate([x0_s, x1_s], axis=1)
     
