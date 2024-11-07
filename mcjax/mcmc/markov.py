2d221f3ecfac258362ec265e022f8b3167d12a4d
Alex Thiery <1282389+alexxthiery@users.noreply.github.com>
2024-11-07 23:12:21 +0800

    add MarkovKernel and Rwm classes for MCMC sampling

---
+++ mcjax/mcmc/markov.py
@@ -0,0 +1,40 @@
+import abc
+import jax
+import jax.numpy as jnp
+import jax.random as jr
+from typing import TypedDict, Tuple, Dict
+from mcjax.proba.density import LogDensity
+
+
+class MarkovKernel(abc.ABC):
+    """ Abstract class for Markov Kernels:
+    - init_state: initialize the state of the kernel
+    - step: perform a single step of the kernel
+    - summarize_stats_traj: summarize the statistics of the kernel trajectory
+    """
+    @abc.abstractmethod
+    def init_state(self, x_init):
+        raise NotImplementedError("Subclasses must implement this method")
+    
+    @abc.abstractmethod
+    def step(self, state, key):
+        raise NotImplementedError("Subclasses must implement this method")
+    
+    @abc.abstractmethod
+    def summarize_stats_traj(self, stats_traj):
+        raise NotImplementedError("Subclasses must implement this method")
+    
+    def run(self, key, n_samples, state_init):
+        def scan_fn(carry, _):
+            """ one step of the MCMC kernel """
+            key, state = carry
+            key, key_ = jr.split(key)
+            state, stats = self.step(state, key_)
+            return (key, state), (state, stats)
+        
+        key, key_ = jr.split(key)
+        _, (state_traj, stats_traj) = jax.lax.scan(scan_fn, (key_, state_init), length=n_samples)
+        stats_summary = self.summarize_stats_traj(stats_traj)
+        
+        # merge the state_trajectory and stats_summary
+        return {**state_traj, **stats_summary}
--- mcjax/mcmc/rwm.py
+++ mcjax/mcmc/rwm.py
@@ -1,25 +1,41 @@
 import jax
 import jax.numpy as jnp
 import jax.random as jr
-
+from typing import TypedDict, Tuple, Dict
 from mcjax.proba.density import LogDensity
+from mcjax.mcmc.markov import MarkovKernel
 
 
-class RWM:
-    """ Random Walk Metropolis-Hastings 
-    Use Gaussian proposal distribution
-    """
+# ==================================
+# Random Walk Metropolis-Hastings
+# with Gaussian proposal distribution
+# ==================================
+class RwmState(TypedDict):
+    """ State storing the current state of the RWM kernel """
+    x: jnp.ndarray
+    logdensity: jnp.ndarray
+
+
+class RwmStats(TypedDict):
+    """ Stores the statistics of RWM steps """
+    sq_jump: jnp.ndarray
+    is_accept: jnp.ndarray
+    accept_MH: jnp.ndarray
+
+
+class Rwm(MarkovKernel):
     def __init__(
                 self,
                 *,
-                logtarget: LogDensity,       # logtarget distribution
-                step_size: float,   # step size
-                cov=None,           # covariance matrix
+                logtarget: LogDensity,  # logtarget distribution
+                step_size: float,       # step size
+                cov=None,               # covariance matrix
                 ):
         self.logtarget = logtarget
         self.step_size = step_size
         self.dim = logtarget.dim
-        # covariance matrix of the proposal distribution
+        
+        # create covariance matrix of the proposals
         if cov is None:
             self.cov = jnp.eye(self.dim)
         elif jnp.ndim(cov) == 1:
@@ -30,49 +46,71 @@ class RWM:
             self.cov = cov
         else:
             raise ValueError("Invalid covariance matrix")
-
-    def sample(
-            self,
-            *,
-            key: jax.Array,             # random key
-            n_samples: int,             # number of samples
-            x_init: jnp.ndarray,        # initial point
-            ):
-        """ sample from the logtarget distribution using RWM """
-
-        # implement a single step of RWM
-        def rwm_step(carry, _):
-            key, x = carry
-            key, key_ = jr.split(key)
-            x_prop = x + jr.normal(key_, (self.dim,)) * self.step_size
-            logtarget_current = self.logtarget(x)
-            logtarget_proposal = self.logtarget(x_prop)
-            log_ratio = logtarget_proposal - logtarget_current
-            # accept or reject
-            key, key_ = jr.split(key)
-            u = jr.uniform(key_)
-            accept_MH = jnp.exp(jnp.minimum(0., log_ratio))
-            # square jump distance statistics
-            sq_jump = accept_MH * jnp.linalg.norm(x_prop - x)**2
-            # metropolis-hastings acceptance
-            is_accept = u < accept_MH
-            x = jnp.where(is_accept, x_prop, x)
-            logtarget = jnp.where(is_accept, logtarget_proposal, logtarget_current)
-            output_dict = {'x': x,
-                           'logtarget': logtarget,
-                           'is_accept': is_accept,
-                           'accept_MH': accept_MH,
-                           'sq_jump': sq_jump,
-                           }
-            return (key, x), output_dict
         
-        # run RWM
+    def init_state(
+            self,
+            x_init: jnp.ndarray,     # initial point
+            ) -> RwmState:
+        """ Initialize the state of the RWM kernel """
+        # check the dimension of the initial point
+        assert x_init.shape == (self.dim,), "Invalid initial point"
+        
+        state = RwmState(x=x_init, logdensity=self.logtarget(x_init))
+        return state
+    
+    def step(
+            self,
+            state: RwmState,    # current state
+            key: jax.Array,     # random key
+            ) -> Tuple[RwmState, RwmStats]:
+        """ Perform a single step of the RWM kernel """                
+        # unpack the state and key
+        x = state['x']
+        logtarget_current = state['logdensity']
+        
+        # create a proposal
         key, key_ = jr.split(key)
-        _, mcmc_trajectory = jax.lax.scan(rwm_step,
-                                          (key_, x_init),
-                                          length=n_samples)
-        # compute statistics
-        mcmc_trajectory['acceptance_rate'] = jnp.mean(mcmc_trajectory['accept_MH'])
-        mcmc_trajectory['n_accepted'] = jnp.sum(mcmc_trajectory['is_accept'])
-        mcmc_trajectory['sq_jump'] = jnp.mean(mcmc_trajectory['sq_jump'])
-        return mcmc_trajectory
+        x_prop = x + jr.normal(key_, (self.dim,)) * self.step_size
+        logtarget_proposal = self.logtarget(x_prop)
+        
+        # accept or reject
+        key, key_ = jr.split(key)
+        u = jr.uniform(key_)
+        log_ratio = logtarget_proposal - logtarget_current
+        accept_MH = jnp.exp(jnp.minimum(0., log_ratio))
+        
+        # square jump distance statistics
+        sq_jump = accept_MH * jnp.linalg.norm(x_prop - x)**2
+        
+        # metropolis-hastings acceptance
+        is_accept = u < accept_MH
+        x_new = jnp.where(is_accept, x_prop, x)
+        logdensity_new = jnp.where(
+                                is_accept,
+                                logtarget_proposal,
+                                logtarget_current)
+        
+        # create the new state
+        state_new = RwmState(x=x_new, logdensity=logdensity_new)
+        
+        # store the statistics
+        statistics = RwmStats(
+                        sq_jump=sq_jump,
+                        is_accept=is_accept,
+                        accept_MH=accept_MH)
+        return state_new, statistics
+    
+    def summarize_stats_traj(
+            self,
+            stats_traj: RwmStats,
+            ) -> Dict:
+        """ Summarize the statistics of the RWM trajectory """
+        acceptance_rate = jnp.mean(stats_traj['accept_MH'])
+        n_accepted = jnp.sum(stats_traj['is_accept'])
+        sq_jump = jnp.mean(stats_traj['sq_jump'])
+        stats_summary = {
+            'acceptance_rate': acceptance_rate,
+            'n_accepted': n_accepted,
+            'sq_jump': sq_jump,
+        }
+        return stats_summary
