4466656a9e6ff3d16d64764328c924790b01a4bc
jimmily98 <lianghangao98@gmail.com>
2025-04-02 11:59:59 +0800

    updated logZ calculation

---
+++ notebooks/sequential_mc/smc_analysis.py
@@ -0,0 +1,95 @@
+import numpy as np  
+
+import jax
+import jax.numpy as jnp
+import jax.random as jr
+import pylab as plt
+
+from typing import TypedDict
+import time
+from scipy.special import logsumexp
+
+# add ../mcjax to the path
+import sys
+sys.path.append('../../')
+
+from mcjax.smc.geometric_smc import GeometricSMC
+from mcjax.proba.gaussian import IsotropicGauss
+from mcjax.proba.neal_funnel import NealFunnel
+from mcjax.proba.student import Student
+from mcjax.proba.banana2d import Banana2D
+
+
+# Functions to calculate the normalizing constant estimator
+def single_run(GSMC:GeometricSMC, num_particles, key, mc_method):
+    t, _, log_weight_arr,*_ = GSMC.selfadaptive_run(num_particles, key, mc_method)
+    return t, log_weight_arr
+
+def mult_run(GSMC:GeometricSMC, num_particles, key, mc_method, num_run):
+    keys = jr.split(key, num_run)
+    batch_run = jax.vmap(single_run, in_axes=(None,None,0,None))
+    batch_run = jax.jit(batch_run, static_argnums=(0,1,3))
+    t_arr, log_weight_batch = batch_run(GSMC,num_particles,keys,mc_method)
+    
+    logZ_arr = [
+        jnp.sum(logsumexp(log_weight_batch[i, :, 1:] - jnp.log(num_particles), axis=0))
+        for i, t in enumerate(t_arr)
+    ]
+
+    return jnp.array(logZ_arr)
+
+def compute_variance(GSMC:GeometricSMC, key, num_particles, method,num_run):
+    t_start = time.time()
+    logZ_arr = mult_run(GSMC, num_particles, key, method, num_run)
+    t_end = time.time() 
+    print(f"Computation time (with jit): {t_end-t_start}")
+
+    # Compute the variance of log(Z)
+    variance = jnp.var(logZ_arr)
+    mean = jnp.mean(logZ_arr)
+    return mean, variance
+
+key = jr.key(0)
+dim = 20
+mu_0 = jnp.zeros(dim)
+sigma_0 = 1.
+log_var_0 = jnp.log(sigma_0**2)
+log_gamma_0 = IsotropicGauss(mu=mu_0, log_var=log_var_0)
+
+
+mu_1 = jnp.ones(dim)
+sigma_1 = 0.3
+log_var_1 = jnp.log(sigma_1**2)
+log_gamma_T = IsotropicGauss(mu=mu_1, log_var=log_var_1)
+
+# log_gamma_T = NealFunnel()
+# log_gamma_T = Banana2D()
+
+num_particles_arr = [50, 100, 500, 1000]
+num_run = 200
+N = 10
+coefs = jnp.arange(N+1)/N
+method = "MALA"
+data1 = {"N_arr": num_particles_arr, "logZ": []}
+for num_particles in num_particles_arr:
+    GSMC = GeometricSMC(log_gamma_0= log_gamma_0, log_gamma_T= log_gamma_T, coefs=coefs, \
+                    step_size=1, num_substeps=10)
+    print("Running with num_particles: ", num_particles)
+    key, key_ = jr.split(key)
+    logZ_arr = mult_run(GSMC, num_particles=num_particles, key=key_, mc_method=method, num_run = num_run)
+    data1["logZ"].append(logZ_arr)
+# plot boxplot of logZ with confidence interval and mean
+plt.figure()
+positions = np.arange(len(num_particles_arr))
+plt.boxplot(data1["logZ"], positions=positions, showmeans=True, meanline=True, notch=True, showfliers=False, whiskerprops=dict(color='orange'))
+
+# compare to logZ of funnel distribution
+plt.axhline(y=log_gamma_T._log_Z - log_gamma_0._log_Z, color='r', linestyle='--', label='True logZ')
+
+plt.xticks(positions, num_particles_arr)
+plt.xlabel('Number of particles')
+plt.ylabel('logZ')
+plt.title(f'Boxplot of logZ with respect to number of particles ({method})')
+plt.legend()
+plt.savefig(f'logZ_{method}.png')
+plt.close()
