61d781ad54a0f67b7d24c3e4772206d1a8f1dd62
jimmily98 <lianghangao98@gmail.com>
2025-05-12 21:21:15 +0800

    add score term

--- mcjax/process/dds.sh
+++ mcjax/process/dds.sh
@@ -3,10 +3,10 @@
 if_train=true
 model_path='model_params.pkl'
 if_animation=true
-if_logZ=true
+add_score=true
 
 python dds.py \
   --if_train $if_train \
   --model_path $model_path \
   --if_animation $if_animation \
-  --if_logZ $if_logZ
+  --add_score $add_score 
