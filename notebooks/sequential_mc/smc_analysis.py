2f9f7999904fda8e294b3414c07194a3ef7eeefc
jimmily98 <lianghangao98@gmail.com>
2025-05-31 09:51:41 +0800

    new test

--- notebooks/sequential_mc/smc_analysis.py
+++ notebooks/sequential_mc/smc_analysis.py
@@ -13,6 +13,7 @@ from scipy.special import logsumexp
 import os
 import sys
 sys.path.append(os.getcwd())
+sys.path.append('../../')
 
 from mcjax.smc.geometric_smc import GeometricSMC
 from mcjax.proba.gaussian import IsotropicGauss,GMM40,MixedIsotropicGauss
