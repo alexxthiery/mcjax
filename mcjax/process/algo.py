2a131d560d174afffcfe898587eaef815211ee1b
jimmily98 <lianghangao98@gmail.com>
2025-06-09 23:01:40 +0800

    add IDEM

--- mcjax/process/algo.py
+++ mcjax/process/algo.py
@@ -570,7 +570,7 @@ class IDEMAlgorithm(BaseAlgorithm):
         )
         
         def outer_step(carry, outer_idx):
-            jax.debug.print("Starting outer loop with {} iterations", outer_idx)
+            jax.debug.print("Starting outer loop {} ", outer_idx)
             key, state, buffer, logz_vals, logz_vars = carry
             
             # Sample new x₀'s and update buffer
