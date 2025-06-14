20424fbc7ee3c31ad5cb9ae31e7b16e3d6de4eee
jimmily98 <lianghangao98@gmail.com>
2025-06-14 14:53:45 +0800

    add IDEM

--- mcjax/process/main.py
+++ mcjax/process/main.py
@@ -119,9 +119,9 @@ def main():
     ########################### -test ###########################
     # plot the buffer
     plt.figure(figsize=(10, 6))
-    raw_sample = alg.buffer.sample.__wrapped__ # grab the raw Python metho
-    data, = raw_sample(alg.buffer,jr.PRNGKey(0), 5000)
-    # convert jnp.array to array
+    with jax.disable_jit():
+        raw = alg.buffer.sample.__wrapped__
+        data, = raw(alg.buffer, jr.PRNGKey(0), 5000)
     data = jax.device_get(data).flatten()
     plt.hist(data, bins=50, density=True, alpha=0.5, label='Buffer Samples')
     # Plot target distribution
