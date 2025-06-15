358f3e866cef9f920fb6062a82ad9cc614c0fbee
jimmily98 <lianghangao98@gmail.com>
2025-06-15 22:19:40 +0800

    add IDEM

--- mcjax/process/algo.py
+++ mcjax/process/algo.py
@@ -662,7 +662,7 @@ class IDEMAlgorithm(BaseAlgorithm):
             if outer_idx % 10 == 0:
                 print(f"in loop {outer_idx} data is of size {self.buffer.size}")
                 data = self.buffer.data
-                size = int(self.buffer.size)
+                size = self.buffer.size.astype(int)
                 print(size)
                 data = data[:size]  # Only take the filled part of the buffer
                 plt.figure(figsize=(10, 6))
