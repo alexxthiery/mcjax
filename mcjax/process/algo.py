f894df4f30861ea97ae10f0041c4a7c088c6f1e8
jimmily98 <lianghangao98@gmail.com>
2025-06-22 21:21:27 +0800

    add PIS

--- mcjax/process/algo.py
+++ mcjax/process/algo.py
@@ -654,7 +654,7 @@ class PISAlgorithm(BaseAlgorithm):
         return jax.jit(score_fn)
 
     def make_loss(self):
-        return PISLoss(add_score=self.cfg.add_score)
+        return PISLoss()
 
     def train(self, rng_key):
         """
