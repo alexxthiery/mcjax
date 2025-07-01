d71af282883b76b2327b1e62b323a711811e3b93
jimmily98 <lianghangao98@gmail.com>
2025-07-01 22:48:17 +0800

    compare score

--- mcjax/process/main.py
+++ mcjax/process/main.py
@@ -154,7 +154,7 @@ def main():
 
     # generate true score_seq for 1d mixed gaussian case
     if args.target_dist == "1d":
-        true_score_seq = jnp.zeros_like(score_seq)
+        true_score_seq = np.zeros_like(score_seq)
         for t in range(args.K):
             y = samples_seq[t, :, 0]
             true_score_seq[t,:] = alg.ou.ou_mixture_score(y, args.K - t - 1,alg.target_dist.mu,\
